83ee88d0f2ad02c0ec23d38e566e4e5e613a5155
Muhammad Khaidir <khaedir3018@gmail.com>
2025-01-10 00:17:57 +0800

    add link portofolio

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -1,36 +1,34 @@
 import Image, { StaticImageData } from "next/image";
 
 export default function Testimonial({
-	testimonial,
-	className,
+  testimonial,
+  className,
 }: {
-	testimonial: {
-		img: StaticImageData;
-		name: string;
-		username?: string;
-	};
-	className?: string;
+  testimonial: {
+    img: StaticImageData;
+    name: string;
+    username?: string;
+    link: string;
+  };
+  className?: string;
 }) {
-	return (
-		<article
-			className={`relative flex justify-center flex-col rounded-2xl bg-white/70 p-8 shadow-lg shadow-black/[0.03] before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] md:odd:-rotate-1 md:even:rotate-1 ${className}`}
-		>
-			<Image
-				className="mx-auto mb-3"
-				src={testimonial.img}
-				alt={testimonial.name}
-			/>
-			<div className="text-center">
-				<div className="font-bold">{testimonial.name}</div>
-				<div>
-					<a
-						className="text-sm font-medium text-gray-500/80 transition hover:text-gray-500"
-						href="#0"
-					>
-						{testimonial.username}
-					</a>
-				</div>
-			</div>
-		</article>
-	);
+  return (
+    <article
+      className={`relative flex justify-center flex-col rounded-2xl bg-white/70 p-8 shadow-lg shadow-black/[0.03] before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(theme(colors.gray.100),theme(colors.gray.200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] md:odd:-rotate-1 md:even:rotate-1 ${className}`}
+    >
+      <Image
+        className="mx-auto mb-3"
+        src={testimonial.img}
+        alt={testimonial.name}
+      />
+      <div className="text-center">
+        <a href={testimonial.link} target="_blank">
+          <div className="font-bold">{testimonial.name}</div>
+          <div className="text-sm font-medium text-gray-500/80 transition hover:text-gray-500">
+            {testimonial.username}
+          </div>
+        </a>
+      </div>
+    </article>
+  );
 }
--- components/testimonials-carousel.tsx
+++ components/testimonials-carousel.tsx
@@ -5,79 +5,83 @@ import TestimonialImg03 from "@/public/images/portofolio3.jpg";
 import TestimonialImg04 from "@/public/images/portofolio4.jpg";
 
 export default function TestimonialsCarousel() {
-	const testimonials = [
-		{
-			img: TestimonialImg01,
-			name: "Brosur PPDB",
-			username: "SMK Negeri 1 Amuntai",
-		},
-		{
-			img: TestimonialImg02,
-			name: "Website Sekolah",
-			username: "SMK Negeri 1 Amuntai",
-		},
-		{
-			img: TestimonialImg03,
-			name: "Aplikasi Android",
-			username: "KEMENAG HSU",
-		},
-		{
-			img: TestimonialImg04,
-			name: "Website Manajamen Toko Servis",
-			username: "CV. Maju Jaya Bahagia",
-		},
-	];
+  const testimonials = [
+    {
+      img: TestimonialImg01,
+      name: "Brosur PPDB",
+      username: "SMK Negeri 1 Amuntai",
+      link: "https://www.instagram.com/p/CaRnHk9PxIP/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+      img: TestimonialImg02,
+      name: "Website Sekolah",
+      username: "SMK Negeri 1 Amuntai",
+      link: "https://smkn1amuntai.sch.id/",
+    },
+    {
+      img: TestimonialImg03,
+      name: "Aplikasi Android",
+      username: "KEMENAG HSU",
+      link: "#",
+    },
+    {
+      img: TestimonialImg04,
+      name: "Website Manajamen Toko Servis",
+      username: "CV. Maju Jaya Bahagia",
+      link: "https://saraba-bisa.com/",
+    },
+  ];
 
-	return (
-		<section className="relative before:pointer-events-none before:absolute before:inset-0 before:-z-10 before:h-[120%] before:bg-gradient-to-b before:from-gray-100">
-			<div className="pt-12 md:pt-20">
-				<div className="mx-auto max-w-6xl px-4 sm:px-6">
-					<div className="mx-auto max-w-3xl text-center">
-						<h2 className="text-3xl font-bold md:text-4xl">Portofolio Kami</h2>
-					</div>
-				</div>
-				<div className="relative mx-auto flex max-w-[94rem] justify-center">
-					<div
-						className="absolute bottom-20 -z-10 -translate-x-36"
-						aria-hidden="true"
-					>
-						<div className="h-80 w-80 rounded-full bg-gradient-to-tr from-blue-500 to-gray-900 opacity-30 blur-[160px] will-change-[filter]" />
-					</div>
-					<div className="absolute -bottom-10 -z-10" aria-hidden="true">
-						<div className="h-80 w-80 rounded-full bg-blue-500 opacity-40 blur-[160px] will-change-[filter]" />
-					</div>
-					<div className="absolute bottom-0 -z-10" aria-hidden="true">
-						<div className="h-56 w-56 rounded-full border-[20px] border-white blur-[20px] will-change-[filter]" />
-					</div>
-					{/* Row */}
-					<div className="group inline-flex w-full flex-nowrap py-12 [mask-image:_linear-gradient(to_right,transparent_0,_black_10%,_black_90%,transparent_100%)] md:py-20">
-						<div className="flex animate-[infinite-scroll_60s_linear_infinite] justify-center group-hover:[animation-play-state:paused] [&>*]:mx-3">
-							{/* Items */}
-							{testimonials.map((testimonial, index) => (
-								<Testimonial
-									key={index}
-									testimonial={testimonial}
-									className="w-[22rem] transition-transform duration-300 group-hover:rotate-0"
-								></Testimonial>
-							))}
-						</div>
-						{/* Duplicated element for infinite scroll */}
-						<div
-							className="flex animate-[infinite-scroll_60s_linear_infinite] justify-center group-hover:[animation-play-state:paused] [&>*]:mx-3"
-							aria-hidden="true"
-						>
-							{/* Items */}
-							{testimonials.map((testimonial, index) => (
-								<Testimonial
-									key={index}
-									testimonial={testimonial}
-									className="w-[22rem] transition-transform duration-300 group-hover:rotate-0"
-								></Testimonial>
-							))}
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className="relative before:pointer-events-none before:absolute before:inset-0 before:-z-10 before:h-[120%] before:bg-gradient-to-b before:from-gray-100">
+      <div className="pt-12 md:pt-20">
+        <div className="mx-auto max-w-6xl px-4 sm:px-6">
+          <div className="mx-auto max-w-3xl text-center">
+            <h2 className="text-3xl font-bold md:text-4xl">Portofolio Kami</h2>
+          </div>
+        </div>
+        <div className="relative mx-auto flex max-w-[94rem] justify-center">
+          <div
+            className="absolute bottom-20 -z-10 -translate-x-36"
+            aria-hidden="true"
+          >
+            <div className="h-80 w-80 rounded-full bg-gradient-to-tr from-blue-500 to-gray-900 opacity-30 blur-[160px] will-change-[filter]" />
+          </div>
+          <div className="absolute -bottom-10 -z-10" aria-hidden="true">
+            <div className="h-80 w-80 rounded-full bg-blue-500 opacity-40 blur-[160px] will-change-[filter]" />
+          </div>
+          <div className="absolute bottom-0 -z-10" aria-hidden="true">
+            <div className="h-56 w-56 rounded-full border-[20px] border-white blur-[20px] will-change-[filter]" />
+          </div>
+          {/* Row */}
+          <div className="group inline-flex w-full flex-nowrap py-12 [mask-image:_linear-gradient(to_right,transparent_0,_black_10%,_black_90%,transparent_100%)] md:py-20">
+            <div className="flex animate-[infinite-scroll_60s_linear_infinite] justify-center group-hover:[animation-play-state:paused] [&>*]:mx-3">
+              {/* Items */}
+              {testimonials.map((testimonial, index) => (
+                <Testimonial
+                  key={index}
+                  testimonial={testimonial}
+                  className="w-[22rem] transition-transform duration-300 group-hover:rotate-0"
+                ></Testimonial>
+              ))}
+            </div>
+            {/* Duplicated element for infinite scroll */}
+            <div
+              className="flex animate-[infinite-scroll_60s_linear_infinite] justify-center group-hover:[animation-play-state:paused] [&>*]:mx-3"
+              aria-hidden="true"
+            >
+              {/* Items */}
+              {testimonials.map((testimonial, index) => (
+                <Testimonial
+                  key={index}
+                  testimonial={testimonial}
+                  className="w-[22rem] transition-transform duration-300 group-hover:rotate-0"
+                ></Testimonial>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
