a41ac9e82529be665fd7f25c0c497dba7e4887aa
Muhammad Khaidir <khaedir3018@gmail.com>
2025-03-20 13:45:34 +0800

    update portofolio

--- components/testimonials-carousel.tsx
+++ components/testimonials-carousel.tsx
@@ -3,20 +3,28 @@ import TestimonialImg01 from "@/public/images/portofolio1.jpg";
 import TestimonialImg02 from "@/public/images/portofolio2.jpg";
 import TestimonialImg03 from "@/public/images/portofolio3.jpg";
 import TestimonialImg04 from "@/public/images/portofolio4.jpg";
+import TestimonialImg05 from "@/public/images/portofolio5.jpg";
+import TestimonialImg06 from "@/public/images/portofolio6.jpg";
 
 export default function TestimonialsCarousel() {
   const testimonials = [
     {
-      img: TestimonialImg01,
-      name: "Brosur PPDB",
-      username: "SMK Negeri 1 Amuntai",
-      link: "https://www.instagram.com/p/CaRnHk9PxIP/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      img: TestimonialImg06,
+      name: "Website Portal Berita",
+      username: "Tirai Kota",
+      link: "https://tiraikota.com/",
     },
     {
-      img: TestimonialImg02,
-      name: "Website Sekolah",
+      img: TestimonialImg05,
+      name: "Website PPDB Sekolah",
       username: "SMK Negeri 1 Amuntai",
-      link: "https://smkn1amuntai.sch.id/",
+      link: "https://ppdb.smkn1amuntai.sch.id/",
+    },
+    {
+      img: TestimonialImg04,
+      name: "Website Manajamen Toko Servis",
+      username: "CV. Maju Jaya Bahagia",
+      link: "https://saraba-bisa.com/",
     },
     {
       img: TestimonialImg03,
@@ -25,10 +33,16 @@ export default function TestimonialsCarousel() {
       link: "#",
     },
     {
-      img: TestimonialImg04,
-      name: "Website Manajamen Toko Servis",
-      username: "CV. Maju Jaya Bahagia",
-      link: "https://saraba-bisa.com/",
+      img: TestimonialImg02,
+      name: "Website Sekolah",
+      username: "SMK Negeri 1 Amuntai",
+      link: "https://smkn1amuntai.sch.id/",
+    },
+    {
+      img: TestimonialImg01,
+      name: "Brosur PPDB",
+      username: "SMK Negeri 1 Amuntai",
+      link: "https://www.instagram.com/p/CaRnHk9PxIP/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
   ];
 
